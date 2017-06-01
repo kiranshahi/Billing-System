56070b5e105859558216b41c9b39b992f587493f
kiranshahi <its.kiran.shahi@gmail.com>
2017-06-01 16:10:13 +0545

    initial commit

---
+++ Billing/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Billing.Startup))]
+namespace Billing
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
